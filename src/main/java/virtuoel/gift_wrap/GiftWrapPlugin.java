f616bcae898622813d983ed5158759a476741cfe
Virtuoel <Virtuoel@users.noreply.github.com>
2023-08-16 03:10:14 +0300

    Classes get remapped. Not methods or fields yet

--- src/main/java/virtuoel/gift_wrap/GiftWrapPlugin.java
+++ src/main/java/virtuoel/gift_wrap/GiftWrapPlugin.java
@@ -1,11 +1,17 @@
 package virtuoel.gift_wrap;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipError;
 
 import org.quiltmc.loader.api.FasterFiles;
 import org.quiltmc.loader.api.LoaderValue;
@@ -18,9 +24,15 @@ import org.quiltmc.loader.api.plugin.QuiltPluginContext;
 import org.quiltmc.loader.api.plugin.QuiltPluginManager;
 import org.quiltmc.loader.api.plugin.gui.PluginGuiTreeNode;
 import org.quiltmc.loader.api.plugin.solver.ModLoadOption;
+import org.quiltmc.loader.impl.launch.common.MappingConfiguration;
+import org.quiltmc.loader.impl.util.SystemProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.mappingio.MappingVisitor;
+import net.fabricmc.mappingio.adapter.MappingNsRenamer;
+import net.fabricmc.mappingio.format.ProGuardReader;
+import net.fabricmc.mappingio.format.Tiny2Reader;
 import net.fabricmc.mappingio.format.TsrgReader;
 import net.fabricmc.mappingio.tree.MappingTree;
 import net.fabricmc.mappingio.tree.MemoryMappingTree;
@@ -76,35 +88,34 @@ public class GiftWrapPlugin implements QuiltLoaderPlugin
 			LOGGER.info("Done");
 		}
 		
-		MemoryMappingTree tree = new MemoryMappingTree();
-		TsrgReader.read(Files.newBufferedReader(tsrg), tree);
+		Path clientMappings = cache.resolve("forge/" + version + "/client.txt");
 		
-		String from = "obf";
-		String to = "srg";
-		IMappingProvider tsrgMappings = (acceptor) -> {
-			for (MappingTree.ClassMapping classDef : tree.getClasses())
-			{
-				String className = classDef.getName(from);
-				acceptor.acceptClass(className, classDef.getName(to));
-				
-				for (MappingTree.FieldMapping field : classDef.getFields())
-				{
-					acceptor.acceptField(new IMappingProvider.Member(className, field.getName(from), field.getDesc(from)), field.getName(to));
-				}
-				
-				for (MappingTree.MethodMapping method : classDef.getMethods())
-				{
-					IMappingProvider.Member methodIdentifier = new IMappingProvider.Member(className, method.getName(from), method.getDesc(from));
-					acceptor.acceptMethod(methodIdentifier, method.getName(to));
-				}
-			}
-		};
+		if (Files.notExists(clientMappings))
+		{
+			LOGGER.info("Getting client.txt");
+			Files.createDirectories(clientMappings.getParent());
+			URL url = new URL("https://piston-data.mojang.com/v1/objects/6c48521eed01fe2e8ecdadbd5ae348415f3c47da/client.txt");
+			Files.copy(url.openStream(), clientMappings);
+			LOGGER.info("Done");
+		}
+		
+		MemoryMappingTree tree = new MemoryMappingTree();
+		
+		ProGuardReader.read(Files.newBufferedReader(clientMappings), "mojang", "official", tree);
+		TsrgReader.read(Files.newBufferedReader(tsrg), new MappingNsRenamer(tree, Map.of("obf", "official")));
+		loadIntermediary(tree);
+		
+		boolean development = Boolean.parseBoolean(System.getProperty(SystemProperties.DEVELOPMENT, "false"));
+		IMappingProvider mappings = createMappingProvider(tree, "mojang", "srg", development ? "named" : "intermediary");
 		
 		TinyRemapper remapper = TinyRemapper.newRemapper()
-			.withMappings(tsrgMappings)
+			.withMappings(mappings)
 			.renameInvalidLocals(false)
 			.ignoreFieldDesc(true)
+			.ignoreConflicts(true)
 			.build();
+		
+		remapper.getEnvironment();
 		
 		Path remappedPath = cache.resolve("forge/remapped/" + meta.id());
 		if (Files.notExists(remappedPath))
@@ -120,7 +131,7 @@ public class GiftWrapPlugin implements QuiltLoaderPlugin
 			outputConsumer.close();
 		}
 		
-		Files.copy(remappedPath, memoryFs.resolve(meta.id()));
+		Files.copy(resourceRoot = remappedPath, memoryFs.resolve(meta.id()));
 		
 		ModLoadOption[] options = new ModLoadOption[metadata.size()];
 		
@@ -130,6 +141,61 @@ public class GiftWrapPlugin implements QuiltLoaderPlugin
 		}
 		
 		return options;
+	}
+	
+	public void loadIntermediary(final MappingVisitor visitor)
+	{
+		final Enumeration<URL> urls;
+		try {
+			urls = MappingConfiguration.class.getClassLoader().getResources("mappings/mappings.tiny");
+		} catch (IOException e) {
+			throw new UncheckedIOException("Error trying to locate mappings", e);
+		}
+		
+		while (urls.hasMoreElements())
+		{
+			final URL url = urls.nextElement();
+			try
+			{
+				final URLConnection connection = url.openConnection();
+				
+				try (final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream())))
+				{
+					Tiny2Reader.read(reader, visitor);
+				}
+			}
+			catch (IOException | ZipError e)
+			{
+				throw new RuntimeException("Error reading " + url, e);
+			}
+		}
+	}
+	
+	public static IMappingProvider createMappingProvider(MemoryMappingTree tree, String classSrc, String src, String dst)
+	{
+		return (acceptor) -> {
+			for (MappingTree.ClassMapping classDef : tree.getClasses())
+			{
+				String className = classDef.getName(classSrc);
+				String dstName = classDef.getName(dst);
+				if (dstName == null) continue;
+				acceptor.acceptClass(className, dstName);
+				
+				for (MappingTree.FieldMapping field : classDef.getFields())
+				{
+					dstName = field.getName(dst);
+					if (dstName == null) continue;
+					acceptor.acceptField(new IMappingProvider.Member(className, field.getName(src), field.getDesc(src)), dstName);
+				}
+				
+				for (MappingTree.MethodMapping method : classDef.getMethods())
+				{
+					dstName = method.getName(dst);
+					if (dstName == null) continue;
+					acceptor.acceptMethod(new IMappingProvider.Member(className, method.getName(src), method.getDesc(src)), dstName);
+				}
+			}
+		};
 	}
 	
 	public QuiltPluginContext context()
